19491d2032322a9e4435179d22065c5d99ea0a2f
fef <owo@fef.moe>
2022-11-29 04:31:22 +0100

    cherry-pick emoji reaction changes

--- app/javascript/flavours/glitch/locales/en.js
+++ app/javascript/flavours/glitch/locales/en.js
@@ -147,7 +147,9 @@ const messages = {
   'home.column_settings.show_direct': 'Show DMs',
 
   'notification.markForDeletion': 'Mark for deletion',
+  'notification.reaction': '{name} reacted to your post',
   'notifications.clear': 'Clear all my notifications',
+  'notifications.column_settings.reaction': 'Reactions:',
   'notifications.marked_clear_confirmation': 'Are you sure you want to permanently clear all selected notifications?',
   'notifications.marked_clear': 'Clear selected notifications',
 
@@ -187,6 +189,8 @@ const messages = {
   'keyboard_shortcuts.bookmark': 'to bookmark',
   'keyboard_shortcuts.toggle_collapse': 'to collapse/uncollapse toots',
   'keyboard_shortcuts.secondary_toot': 'to send toot using secondary privacy setting',
+
+  'tooltips.reactions': 'Reactions',
 };
 
 export default Object.assign({}, inherited, messages);
